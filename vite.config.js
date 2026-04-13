5d42051e39d920df590ab74e77b93efec81abdd5
sortex-001 <fredroque043@gmail.com>
2026-04-13 03:48:38 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedex/',
+  base: process.env.DEPLOY_BASE_URL || '/',
   plugins: [
     react(),
     tailwindcss()
